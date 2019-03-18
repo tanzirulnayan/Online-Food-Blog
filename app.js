7ae131e612f625ea9ecbb7a8acdfaff6eb41a91f
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-03-18 16:37:17 +0600

    signup controller done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express 		= require('express');
 var bodyParser 		= require('body-parser');
 var exSession 		= require('express-session');
 var cookieParser 	= require('cookie-parser');
+var signup			= require('./controllers/signup');
 var login			= require('./controllers/login');
 var admin			= require('./controllers/admin');
 var member 			= require('./controllers/member');
@@ -17,11 +18,13 @@ app.set('view engine', 'ejs');
 app.use(exSession({secret: 'my top secret code', saveUninitialized: true, resave: false}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use('/signup', signup);
 app.use('/login', login);
 app.use('/admin', admin);
 app.use('/member', member);
 app.use('/logout', logout);
 app.use('/assets', express.static('ext'));
+app.use('/pictures', express.static('images'));
 
 //ROUTES
 app.get('/', (req,res)=>res.send('Index page'));
---
+++ controllers/signup.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var userModel = require.main.require('./model/user-model');
+var adminModel = require.main.require('./model/admin-model');
+var memberModel = require.main.require('model/member-model');
+var router = express.Router();
+
+router.get('/', (req, res)=>{
+	res.render('signup/index');
+});
+
+router.get('/member', (req, res)=>{
+	res.render('signup/member');
+});
+
+router.get('/admin', (req, res)=>{
+	res.render('signup/admin');
+});
+
+router.post('/member', (req, res)=>{	
+	if(req.body.password == req.body.conPassword)
+	{
+		var user={
+			uname 	 : req.body.username,
+			password : req.body.password,
+            type 	 : "MEMBER",
+            status   : "VALID"
+		};
+		userModel.insert(user, function(success){
+			if(success){
+				var  member = {
+					id		:req.body.username,
+					name	:req.body.name,
+					address :req.body.address,
+					email 	:req.body.email,
+					image	:"/pictures/" + res.req.file.filename
+				}; 
+				memberModel.insert(member, function(success){
+					if(success){
+						res.redirect('/login');
+					}
+					else{
+						res.redirect('/signup/member');
+					}
+				});
+			}
+			else{
+				res.redirect('/signup/member');
+			}
+		});	
+	}
+	else
+	{
+		res.redirect('signup/member');
+	}
+});
+
+router.post('/admin', (req, res)=>{	
+	if(req.body.password == req.body.conPassword)
+	{
+		var user={
+			uname 	 : req.body.username,
+			password : req.body.password,
+            type 	 : "ADMIN",
+            status   : "INVALID"
+		};
+		userModel.insert(user, function(success){
+			if(success){
+				var  admin = {
+					id		:req.body.username,
+					name	:req.body.name,
+					address :req.body.address,
+					email 	:req.body.email,
+					image	:"/pictures/" + res.req.file.filename
+				}; 
+				adminModel.insert(admin, function(success){
+					if(success){
+						res.redirect('/login');
+					}
+					else{
+						res.redirect('/signup/admin');
+					}
+				});
+			}
+			else{
+				res.redirect('/signup/admin');
+			}
+		});	
+	}
+	else
+	{
+		res.redirect('signup/admin');
+	}
+});
+
+module.exports = router;

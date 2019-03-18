48ec36d8549eb9fab39bca3fa406d0a9ee28e07d
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-03-18 13:40:54 +0600

    member-model added

---
+++ model/member-model.js
@@ -0,0 +1,38 @@
+var db = require('./db');
+
+module.exports={
+	get: function(memberId, callback){
+		var sql = "select * from members where M_ID = ?";
+
+		db.getResult(sql, [memberId], function(result){
+			callback(result);
+		});
+	},
+	getAll: function(callback){
+		var sql = "select * from members";
+		db.getResult(sql, [], function(results){
+			callback(results);
+		});
+	},
+	insert: function(member, callback){
+		var sql = "insert into members values (?, ?, ?, ?, ?)";
+		db.execute(sql, [member.id, member.name, member.address, member.email, member.image], function(status){
+			callback(status);
+		});
+	},
+	update: function(user, callback){
+		var sql = "update members set M_NAME = ?, M_ADDRESS = ?, M_EMAIL = ?, M_IMAGE = ? where M_ID = ?";
+		db.execute(sql, [member.name, member.address, member.email, member.image, member.id], function(status){
+			callback(status);
+		});
+	},
+	delete: function(memberId, callback){
+		var sql = "delete from members where M_ID = ?";
+		db.execute(sql, [memberId], function(status){
+			callback(status);
+		});
+	}
+}
+
+
+
